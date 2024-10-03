328946692915966c74bbe6100d0164c312789f36
Danish109-code <mohddanish.gjimt@gmail.com>
2024-10-03 21:22:40 +0530

    Add files via upload

---
+++ homework/script.js
@@ -0,0 +1,46 @@
+﻿// 1. program to check if the number is even or odd
+
+// const num = (prompt("Enter a number: ");
+
+// if(num % 2 == 0) {
+//    console.log(num,"number is even.");
+//}
+
+//else {
+//    console.log(num,"number is odd.");
+//}
+
+
+// 2. program that checks if the number is positive, negative or zero
+
+//const num = prompt("Enter a number: ");
+
+//if (num > 0) {
+//    console.log(num,"is positive number");
+//}
+
+//else if (num == 0) {
+//  console.log(num,"is zero number");
+//}
+
+//else {
+//     console.log(num,"is negative number");
+//}
+
+
+
+// 3. Program to find the largest among three numbers
+
+const num1 = prompt("Enter first number: ");
+const num2 = prompt("Enter second number: ");
+const num3 = prompt("Enter third number: ");
+
+if(num1 >= num2 && num1 >= num3) {
+    console.log(num1,"is greater");
+}
+else if (num2 >= num1 && num2 >= num3) {
+    console.log(num2,"is greater");
+}
+else {
+     console.log(num3," is greater");
+}
